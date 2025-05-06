7609b956b269c21d2967d7b56ff1fd579d4788ea
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-05-06 09:54:42 +0530

    Update ArrayList.java

--- Part 6/ArrayList.java
+++ Part 6/ArrayList.java
@@ -1,16 +1,26 @@
 import java.util.*;
 import java.util.ArrayList;
-
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        sc.nextLine(); 
-        ArrayList<String> arr = new ArrayList<String>();
-        for (int i = 0; i < n; i++) {
-            String s = sc.nextLine();
-            arr.add(s);
-        }
-        System.out.println(arr);
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+		ArrayList<String> arr=new ArrayList<String>();
+		sc.nextLine();
+		for (int i=0;i<n;i++){
+		    String s=sc.nextLine();
+		    arr.add(s);
+		}
+		arr.add(0,"Nice");
+		System.out.println(arr);
+		System.out.println(arr.get(3));
+		arr.set(3,"or");
+		System.out.println(arr);
+		arr.remove(0);
+		System.out.println(arr);
+		System.out.println(arr.size());
+		Collections.sort(arr);
+		System.out.println(arr);
+		
+	}
 }
