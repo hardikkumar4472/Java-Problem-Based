1d972e56b4fab49d81aee1eb2563dfaf9a055e72
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-05-06 10:22:01 +0530

    Create HashMap.java

---
+++ Part 6/HashMap.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Main{
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        HashMap<int,int> hm=new HashMap<int,int>();
+        hm.put<1,2>;
+        hm.get(1);
+        hm.remove(1);
+        hm.clear();
+        hm.size();
+    }
+}
